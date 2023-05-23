483fdf2db6c87eb8781dea978ec57cf62f3c3511
RANDOM <vuquyettam@sohagame.vn>
2023-05-23 11:56:41 +0700

    Add new resource

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable @next/next/no-sync-scripts */
-
+import Image from 'next/image';
 export default function Page() {
   return (
     <>
       <main>
-        
+        <Image
+          alt="Background landing"
+          src="/project/pX9TNcP/both/img/landing202305/bg-landing.jpg"
+          width="1920"
+          height="100"
+          decoding="async"
+          data-nimg="1"
+          className="hidden h-[100%] w-[100vw] bg-cover md:block"
+        />
+        <Image
+          alt="Background landing"
+          src="/project/pX9TNcP/both/img/landing202305/mobile/bg-mobile.jpg"
+          width="1920"
+          height="100"
+          decoding="async"
+          data-nimg="1"
+          className="h-[100%] w-[100%] bg-cover md:hidden"
+        />
       </main>
     </>
   );
